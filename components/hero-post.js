ff92da0824ee9251e5d4d0f51184029b407d155f
Hyunjin <p.hyunjinz@gmail.com>
2020-08-21 11:14:57 +0900

    added tags

--- components/hero-post.js
+++ components/hero-post.js
@@ -18,6 +18,7 @@ export default function HeroPost({
       </div>
       <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
         <div>
+          <p>#beauty</p>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
             <Link as={`/posts/${slug}`} href="/posts/[slug]">
               <a className="hover:underline">{title}</a>
@@ -33,5 +34,5 @@ export default function HeroPost({
         </div>
       </div>
     </section>
-  )
+  );
 }
--- components/post-preview.js
+++ components/post-preview.js
@@ -16,6 +16,7 @@ export default function PostPreview({
       <div className="mb-5">
         <CoverImage slug={slug} title={title} url={coverImage} />
       </div>
+      <p>#beauty</p>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a className="hover:underline">{title}</a>
@@ -27,5 +28,5 @@ export default function PostPreview({
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
       <Avatar name={author.name} picture={author.picture} />
     </div>
-  )
+  );
 }
