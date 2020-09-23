28f783e83cc1329df6317fe76104020b199af356
Hyunjin <p.hyunjinz@gmail.com>
2020-09-23 23:13:21 +0900

    Added margin to header

--- components/header.js
+++ components/header.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <h2>
+    <h2 className="mt-8">
       <Link href="/">
-        <a className="hover:underline tracking-widest font-titleCustom text-2xl md:text-4xl font-bold leading-tight mb-20 mt-20">staytuned</a>
+        <a className="hover:underline tracking-widest font-titleCustom text-2xl md:text-4xl font-bold leading-tight mb-20 mt-8">staytuned</a>
       </Link>
       .
     </h2>
