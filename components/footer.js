d9c199d15133da356034f91474dcfd1925f6e23d
Hyunjin <p.hyunjinz@gmail.com>
2020-09-23 22:57:55 +0900

    Editied some designs...

--- components/footer.js
+++ components/footer.js
@@ -10,13 +10,13 @@ export default function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="http://staytuned.kr"
+              href="https://staytuned.kr/signup/brand"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               입점 시작하기
             </a>
             <a
-              href="http://staytuned.kr"
+              href="https://docs.google.com/forms/d/14B0AK8QgcjqoXTT-X4H22JCvmDznir7pjULUXQnpWfY/edit"
               className="mx-3 font-bold hover:underline"
             >
               입점 상담하기
--- components/header.js
+++ components/header.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <h2 className="tracking-widest font-titleCustom text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+    <h2>
       <Link href="/">
-        <a className="hover:underline tracking-widest font-titleCustom text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">staytuned</a>
+        <a className="hover:underline tracking-widest font-titleCustom text-2xl md:text-4xl font-bold leading-tight mb-20 mt-8">staytuned</a>
       </Link>
       .
     </h2>
