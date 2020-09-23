15fc10318c05f9150924cc726719420a4488ecc8
Hyunjin <p.hyunjinz@gmail.com>
2020-09-23 23:03:06 +0900

    Design fix

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <h2>
       <Link href="/">
-        <a className="hover:underline tracking-widest font-titleCustom text-2xl md:text-4xl font-bold leading-tight mb-20 mt-8">staytuned</a>
+        <a className="hover:underline tracking-widest font-titleCustom text-2xl md:text-4xl font-bold leading-tight mb-20 mt-20">staytuned</a>
       </Link>
       .
     </h2>
