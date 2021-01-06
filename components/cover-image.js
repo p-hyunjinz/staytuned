68e6c49496ed909518500f208d2e442473d565e9
Hyunjin <p.hyunjinz@gmail.com>
2021-01-06 20:05:10 +0900

    Margin fixed

--- components/cover-image.js
+++ components/cover-image.js
@@ -20,7 +20,7 @@ export default function CoverImage({ title, url, slug }) {
   );
 
   return (
-    <div className="mx-5 sm:mx-0">
+    <div className="mx-0 sm:mx-0">
       {slug ? (
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a aria-label={title}>{image}</a>
